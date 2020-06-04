a131052750ac65a0cfcf2674b4bfd29a43143d0e
Anastasiia_Einman <61709672+Anastasiia799@users.noreply.github.com>
2020-06-04 10:59:49 +0300

    2
    lab2

---
+++ TRASH.WebService/ApplicationServices/GetTRASHListUseCase/GetTRASHListUseCaseResponse.cs
@@ -0,0 +1,15 @@
+﻿using TRASH.DomainObjects;
+using TRASH.ApplicationServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TRASH.ApplicationServices.GetYardAreaListUseCase
+{
+    public class GetTRASHListUseCaseResponse : UseCaseResponse
+    {
+        public IEnumerable<trash> TRASHs { get; }
+
+        public GetTRASHListUseCaseResponse(IEnumerable<trash> trashs) => TRASHs = trashs;
+    }
+}
---
+++ TRASH.WebService/ApplicationServices/Ports/IOutputPort.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TRASH.ApplicationServices.Ports
+{
+    public interface IOutputPort<in TUseCaseResponse>
+    {
+        void Handle(TUseCaseResponse response);
+    }
+}
