72061d694a9e619b577cd643f46dc882b76b23d9
Anastasiia_Einman <61709672+Anastasiia799@users.noreply.github.com>
2020-06-04 11:05:04 +0300

    6
    lab6

---
+++ TRASH.DesktopClient/InfrastructureServices/Repositories/NetworkTRASHRepository.cs
@@ -0,0 +1,47 @@
+﻿using TRASH.ApplicationServices.Ports.Cache;
+using TRASH.DomainObjects;
+using TRASH.DomainObjects.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace TRASH.InfrastructureServices.Repositories
+{
+    public class NetworkTRASHRepository : NetworkRepositoryBase, IReadOnlyTRASHRepository
+    {
+        private readonly IDomainObjectsCache<trash> _trashCache;
+
+        public NetworkTRASHRepository(string host, ushort port, bool useTls, IDomainObjectsCache<trash> trashCache)
+            : base(host, port, useTls)
+            => _trashCache = trashCache;
+
+        public async Task<trash> GetTRASH(long id)
+            => CacheAndReturn(await ExecuteHttpRequest<trash>($"trashs/{id}"));
+
+        public async Task<IEnumerable<trash>> GetAllTRASHs()
+            => CacheAndReturn(await ExecuteHttpRequest<IEnumerable<trash>>($"trashs"), allObjects: true);
+
+        public async Task<IEnumerable<trash>> QueryTRASHs(ICriteria<trash> criteria)
+            => CacheAndReturn(await ExecuteHttpRequest<IEnumerable<trash>>($"trashs"), allObjects: true)
+               .Where(criteria.Filter.Compile());
+
+
+        private IEnumerable<trash> CacheAndReturn(IEnumerable<trash> trashs, bool allObjects = false)
+        {
+            if (allObjects)
+            {
+                _trashCache.ClearCache();
+            }
+            _trashCache.UpdateObjects(trashs, DateTime.Now.AddDays(1), allObjects);
+            return trashs;
+        }
+
+        private trash CacheAndReturn(trash trash)
+        {
+            _trashCache.UpdateObject(trash, DateTime.Now.AddDays(1));
+            return trash;
+        }
+    }
+}
---
+++ TRASH.GeneralLogic/ApplicationServices/GetTRASHListUseCase/GetTRASHListUseCaseRequest.cs
@@ -0,0 +1,30 @@
+﻿using TRASH.ApplicationServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TRASH.ApplicationServices.GetYardAreaListUseCase
+{
+    public class GetTRASHListUseCaseRequest : IUseCaseRequest<GetTRASHListUseCaseResponse>
+    {
+        public string YardArea { get; private set; }
+        public long? TRASHId { get; private set; }
+
+        private GetTRASHListUseCaseRequest()
+        { }
+
+        public static GetTRASHListUseCaseRequest CreateAllTRASHsRequest()
+        {
+            return new GetTRASHListUseCaseRequest();
+        }
+
+        public static GetTRASHListUseCaseRequest CreateTRASHRequest(long trashId)
+        {
+            return new GetTRASHListUseCaseRequest() { TRASHId = trashId };
+        }
+        public static GetTRASHListUseCaseRequest CreateTRASHsRequest(string yardarea)
+        {
+            return new GetTRASHListUseCaseRequest() { YardArea = yardarea };
+        }
+    }
+}
